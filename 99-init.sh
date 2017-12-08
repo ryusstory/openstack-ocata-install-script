6b9967e939164b3be8a36016817745d99a12bae0
Young moo Ryu <ryusstory@gmail.com>
2017-12-08 11:13:36 +0900

    fix security rule

--- 99-init.sh
+++ 99-init.sh
@@ -8,18 +8,20 @@
 
 openstack network create --share --external --provider-physical-network provider --provider-network-type flat provider
 openstack subnet create --network provider --allocation-pool start=192.168.0.101,end=192.168.0.199 --dns-nameserver 168.126.63.1 --gateway 192.168.0.1 --subnet-range 192.168.0.0/24 provider
-openstack network create selfservice
+openstack network create --share selfservice
 openstack subnet create --network selfservice --allocation-pool start=10.0.0.101,end=10.0.0.199 --dns-nameserver 168.126.63.1 --gateway 10.0.0.1 --subnet-range 10.0.0.0/24 selfservice
 
 openstack router create router
 neutron router-interface-add router selfservice
 neutron router-gateway-set router provider
-openstack flavor create --vcpus 1 --ram 64 --disk 1 m1.nano
+openstack flavor create --vcpus 1 --ram 256 --disk 1 m1.nano
 
-curl -O http://download.cirros-cloud.net/0.4.0/cirros-0.4.0-x86_64-disk.img
-openstack image create "cirros" --file cirros-0.4.0-x86_64-disk.img --disk-format qcow2 --container-format bare --public
-openstack security group rule create --proto icmp default
-openstack security group rule create --proto tcp --dst-port 22 default
+curl -O http://download.cirros-cloud.net/0.3.5/cirros-0.3.5-x86_64-disk.img
+openstack image create "cirros" --file cirros-0.3.5-x86_64-disk.img --disk-format qcow2 --container-format bare --public
+
+openstack security group create initrule
+openstack security group rule create --protocol icmp $(openstack security group show initrule | grep -e " id " | awk '{print $4}')
+openstack security group rule create --protocol tcp --dst-port 22 $(openstack security group show initrule | grep -e " id " | awk '{print $4}')
 openstack keypair create --public-key ~/.ssh/id_rsa.pub mykey
-openstack server create --flavor m1.nano --image cirros --nic net-id=$(openstack network show selfservice | grep " id " | awk '{print $4}') --security-group default --key-name mykey selfservice-instance
+openstack server create --flavor m1.nano --image cirros --nic net-id=$(openstack network show selfservice | grep " id " | awk '{print $4}') --security-group initrule --key-name mykey selfservice-instance
 openstack server add floating ip selfservice-instance $(openstack floating ip create provider | grep floating_ip_address | awk '{print $4}')
