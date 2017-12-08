aac537a67a83b88aa0316ff2da5c71094d5c9930
Young moo Ryu <ryusstory@gmail.com>
2017-12-08 16:38:39 +0900

    heat doesn't contain [keystone_authtoken]

--- 55-heat.sh
+++ 55-heat.sh
@@ -42,12 +42,25 @@ openstack role create heat_stack_user
 PKGS='openstack-heat-api openstack-heat-api-cfn openstack-heat-engine'
 if [ $QUIETYUM -eq 1 ]; then yum install -q -y $PKGS; else yum install -y $PKGS; fi
 
+cp /etc/heat/heat.conf /etc/heat/backup.heat.conf
+sed -i '/^#/d' /etc/heat/heat.conf
+sed -i '/^$/d' /etc/heat/heat.conf
+
 sed -i "/\[database\]/a connection = mysql+pymysql://heat:$HEAT_DBPASS@${HOST_name[0]}/heat" /etc/heat/heat.conf
 sed -i "/\[DEFAULT\]/a transport_url = rabbit://openstack:$RABBIT_PASS@${HOST_name[0]}\nheat_metadata_server_url = http://${HOST_name[0]}:8000\nheat_waitcondition_server_url = http://${HOST_name[0]}:8000/v1/waitcondition\nstack_domain_admin = heat_domain_admin\nstack_domain_admin_password = $HEAT_DOMAIN_PASS\nstack_user_domain_name = heat" /etc/heat/heat.conf
-sed -i "/\[keystone_authtoken\]/a auth_uri = http://${HOST_name[0]}:5000\nauth_url = http://${HOST_name[0]}:35357\nmemcached_servers = ${HOST_name[0]}:11211\nauth_type = password\nproject_domain_name = default\nuser_domain_name = default\nproject_name = service\nusername = heat\npassword = $HEAT_PASS" /etc/heat/heat.conf
 sed -i "/\[trustee\]/a auth_type = password\nauth_url = http://${HOST_name[0]}:35357\nusername = heat\npassword = $HEAT_PASS\nuser_domain_name = default" /etc/heat/heat.conf
 sed -i "/\[clients_keystone\]/a auth_uri = http://${HOST_name[0]}:35357" /etc/heat/heat.conf
 sed -i "/\[ec2authtoken\]/a auth_uri = http://${HOST_name[0]}:5000" /etc/heat/heat.conf
+#sed -i "/\[keystone_authtoken\]/a auth_uri = http://${HOST_name[0]}:5000\nauth_url = http://${HOST_name[0]}:35357\nmemcached_servers = ${HOST_name[0]}:11211\nauth_type = password\nproject_domain_name = default\nuser_domain_name = default\nproject_name = service\nusername = heat\npassword = $HEAT_PASS" /etc/heat/heat.conf
+echo "auth_uri = http://${HOST_name[0]}:5000
+auth_url = http://${HOST_name[0]}:35357
+memcached_servers = ${HOST_name[0]}:11211
+auth_type = password
+project_domain_name = default
+user_domain_name = default
+project_name = service
+username = heat
+password = $HEAT_PASS" >> /etc/heat/heat.conf
 
 su -s /bin/sh -c "heat-manage db_sync" heat
 
